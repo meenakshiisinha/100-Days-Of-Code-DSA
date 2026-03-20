09df2448624ff0f420f1fe34528d8d5642bd4d61
meenakshiisinha <meenakshisinha755@gmail.com>
2026-03-20 17:00:00 +0530

    Completed Day 048

---
+++ Day 048/Q48.c
@@ -0,0 +1,52 @@
+/*Problem: Count Leaf Nodes
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+int countLeafNodes(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+
+    int leftCount = countLeafNodes(root->left);
+    int rightCount = countLeafNodes(root->right);
+
+    return leftCount + rightCount;
+}
+
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    root->right->left = createNode(6);
+    root->right->right = createNode(7);
+
+    printf("Number of leaf nodes: %d\n", countLeafNodes(root));
+
+    return 0;
+}
+
