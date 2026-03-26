e53612793c2eca9e3986496228c09ba3c7793fd5
meenakshiisinha <meenakshisinha755@gmail.com>
2026-03-26 17:00:00 +0530

    Completed Day 054

---
+++ Day 054/Q54.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+void zigzagTraversal(struct Node* root) {
+    if(root == NULL) return;
+
+    struct Node* q[1000];
+    int front = 0, rear = 0;
+    q[rear++] = root;
+
+    int leftToRight = 1;
+
+    while(front < rear) {
+        int size = rear - front;
+        int level[1000];
+
+        for(int i = 0; i < size; i++) {
+            struct Node* temp = q[front++];
+            int index;
+
+            if(leftToRight)
+                index = i;
+            else
+                index = size - i - 1;
+
+            level[index] = temp->data;
+
+            if(temp->left != NULL)
+                q[rear++] = temp->left;
+            if(temp->right != NULL)
+                q[rear++] = temp->right;
+        }
+
+        for(int i = 0; i < size; i++)
+            printf("%d ", level[i]);
+
+        leftToRight = !leftToRight;
+    }
+}
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    root->right->left = createNode(6);
+    root->right->right = createNode(7);
+
+    printf("Zigzag Traversal: ");
+    zigzagTraversal(root);
+
+    return 0;
+}
